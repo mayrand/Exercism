5c9b2e2d839e8c9f4640f20716b791e5673aaa47
Andrzej Oldak <andrzej.oldak@schange.com>
2019-03-08 12:46:50 +0100

    triangle

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -7,7 +7,7 @@ export default class Triangle {
     }
 
     kind() {
-        if (!this.sides.every(x => x > 0)) {
+        if (!this.sides.every(x => x > 0) || this.sides[0] + this.sides[1] < this.sides[2] || this.sides[0] + this.sides[2] < this.sides[1] || this.sides[1] + this.sides[2] < this.sides[0]) {
             throw "Incorrect triangle!"
         }
 
