ada2f6a0dd4ff64091fe0f346cc47870f6cdde5b
Andrzej Ołdak <andrzej.oldak@schange.com>
2020-01-16 09:32:35 +0100

    Resistor color duo fixes

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -4,13 +4,7 @@
 //
 
 export const decodedValue = (args) => {
-  let output = "";
-  for (let index = 0; index < args.length && index <= 1; index++) {
-    output += colorCode(args[index]).toString();
-  }
-  return parseInt(output);
+  return COLORS.indexOf(args[0]) * 10 + COLORS.indexOf(args[1]);
 };
-
-const colorCode = (name) => COLORS.indexOf(name);
 
 const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
