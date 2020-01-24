b99a9b1c18e9d5ed42a7bc37dcc26cc8f8fa8a06
Andrzej Ołdak <mayrand@tlen.pl>
2020-01-24 14:31:25 +0100

    Gigasecond fin fix

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const gigasecond = (date) => {
-  return new Date(new Date(date).setUTCSeconds(1000000000 + date.getUTCSeconds()));
+  return new Date(1000000000000 + date.getTime());
 };
