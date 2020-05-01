2da81fef0fc680d4f45d52d8c296d1e8a293dadc
Andrzej Ołdak <andrzej.oldak@gmail.com>
2020-05-01 11:40:46 +0200

    Reverse string v2

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -3,6 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const reverseString = (str) => {
-  return str.split("").reverse().join("");
+export const reverseString = (input) => {
+    if (input === '')
+      return '';
+    else
+      return reverseString(input.substr(1)) + input.charAt(0); 
 };
